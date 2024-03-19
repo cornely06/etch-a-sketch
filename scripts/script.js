574026daed344a819823b83bd45177ab6376037b
rc <59358349+cornely06@users.noreply.github.com>
2024-03-19 17:55:34 -0400

    Add event listener to fill cells on hover

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,10 @@ let container = document.querySelector(".container");
 for (i = 0; i < 256; i++) {
   let cell = document.createElement("div");
   cell.classList.add("cell");
+  cell.addEventListener("mouseover", fillCell);
   container.appendChild(cell);
+}
+
+function fillCell() {
+  this.classList.add("filled");
 }
