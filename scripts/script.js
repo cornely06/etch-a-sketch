d37417583b239ca85b1a2086eadc401b045bd1c0
rc <59358349+cornely06@users.noreply.github.com>
2024-03-19 19:59:14 -0400

    Add button that resizes cells based off user input

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,43 @@
 let container = document.querySelector(".container");
+let buttons = document.querySelector(".buttons");
 init();
 
 function init() {
   for (i = 0; i < 256; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.style.width = "40px";
+    cell.style.height = "40px";
+    cell.addEventListener("mouseover", fillCell);
+    container.appendChild(cell);
+  }
+
+  let adjustSize = document.createElement("button");
+  adjustSize.textContent = "Adjust size of grid";
+  adjustSize.addEventListener("click", customGrid);
+  buttons.appendChild(adjustSize);
+}
+
+function fillCell() {
+  this.classList.add("filled");
+}
+
+function customGrid() {
+  let size = Number(prompt("Enter a number"));
+  clearContainer();
+
+  for (i = 0; i < size ** 2; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.width = 640 / size + "px";
+    cell.style.height = 640 / size + "px";
     cell.addEventListener("mouseover", fillCell);
     container.appendChild(cell);
   }
 }
 
-function fillCell() {
-  this.classList.add("filled");
+function clearContainer() {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
 }
